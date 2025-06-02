ca4879ebf84de4a7db6533027047958d57db1cab
kaleb22 <kaleb22@hotmail.com>
2025-06-02 13:35:46 -0300

    feat(template-driven): impl form validations using template driven approach

--- src/app/login/template-driven/template-driven.component.ts
+++ src/app/login/template-driven/template-driven.component.ts
@@ -1,9 +1,15 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven',
-  imports: [],
+  imports: [FormsModule, NgClass],
   templateUrl: './template-driven.component.html',
   styleUrl: './template-driven.component.scss',
 })
-export class TemplateDrivenComponent {}
+export class TemplateDrivenComponent {
+  onSubmit(formData: NgForm) {
+    formData.form.reset();
+  }
+}
