09adb517bc6cbb840afcdf15465fc7163027fde4
kaleb22 <kaleb22@hotmail.com>
2025-06-02 09:27:34 -0300

    feature(reactive): creating html and css structure for reactive forms component

---
+++ src/app/login/reactive/reactive.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive',
+  imports: [],
+  templateUrl: './reactive.component.html',
+  styleUrl: './reactive.component.scss',
+})
+export class ReactiveComponent {}
