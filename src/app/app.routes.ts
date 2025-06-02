952cdea652b4087584478dc32026081f331d1ef5
kaleb22 <kaleb22@hotmail.com>
2025-06-02 09:22:12 -0300

    feature(routes): creating home and template-driven component to configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TemplateDrivenComponent } from './login/template-driven/template-driven.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'template-driven',
+    component: TemplateDrivenComponent,
+  },
+];
---
+++ src/app/login/template-driven/template-driven.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven',
+  imports: [],
+  templateUrl: './template-driven.component.html',
+  styleUrl: './template-driven.component.scss',
+})
+export class TemplateDrivenComponent {}
