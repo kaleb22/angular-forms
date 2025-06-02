77ccdcbf8fc273f77a9aa214c471f5eb9122ec4d
kaleb22 <kaleb22@hotmail.com>
2025-06-02 09:34:10 -0300

    feat(routes): setup reactive-forms route and naming forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TemplateDrivenComponent } from './login/template-driven/template-driven.component';
 import { HomeComponent } from './home/home.component';
+import { ReactiveComponent } from './login/reactive/reactive.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,9 @@ export const routes: Routes = [
   {
     path: 'template-driven',
     component: TemplateDrivenComponent,
+  },
+  {
+    path: 'reactive-forms',
+    component: ReactiveComponent,
   },
 ];
